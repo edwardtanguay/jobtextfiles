216f3541600003cd64a9e7c4a107be439b57ff97
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-20 11:11:18 +0100

    showing job titles

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ import * as tools from './tools.js';
 
 const jobs = tools.getJobs();
 
-console.log(`there are ${jobs.length} jobs`);
+console.log('HR JOB REPORT\n');
+console.log(`There are ${jobs.length} jobs:\n`);
+
+for (let i = 0; i < jobs.length; i++) {
+	const job = jobs[i];
+	console.log(job.title);
+}
