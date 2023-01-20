7762fe7e83f8fb88ff8ddcbb1ce35c54ff07ff6a
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-20 13:36:27 +0100

    testing createReport function

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ import * as tools from './tools.js';
 const jobs = tools.getJobs();
 const skills = tools.getSkills();
 
-console.log('HR JOB REPORT\n');
-console.log(`There are ${jobs.length} jobs:\n`);
+const createReport = () => {
+	let r = '';
+	r += 'HR JOB REPORT\n';
+	r += `There are ${jobs.length} jobs:\n`;
 
-for(const job of jobs) {
-	console.log(job.title);
-	console.log(job.url);
-	const skillIdCodes = job.skillList.split(',');
-	for (let skillIdCode of skillIdCodes) {
-		skillIdCode = skillIdCode.trim();
-		const skill = tools.lookupObjectInSkills(skills, skillIdCode);
-		// const skill = skills.find(m => m.idCode === skillIdCode); // higher-order-function solution
-		console.log(`- ${skill.name} - ${skill.description}`);
+	for (const job of jobs) {
+		r += job.title + '\n';
+		r += job.url + '\n';
+		const skillIdCodes = job.skillList.split(',');
+		for (let skillIdCode of skillIdCodes) {
+			skillIdCode = skillIdCode.trim();
+			const skill = tools.lookupObjectInSkills(skills, skillIdCode);
+			// const skill = skills.find(m => m.idCode === skillIdCode); // higher-order-function solution
+			r += `- ${skill.name} - ${skill.description}\n`;
+		}
+		r += '\n';
 	}
-	console.log('')
-}
+
+	return r;
+};
+
+const report = createReport();
+console.log(report);
